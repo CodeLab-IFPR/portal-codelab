ddb42c7088fb6bb411a6f20f9297921d40b5b771
Carlos Eduardo Mariano Garcia Pereira <69264602+caduHD4@users.noreply.github.com>
2024-08-08 21:14:57 -0300

    Revert "Develop template"

--- routes/web.php
+++ routes/web.php
@@ -1,24 +1,10 @@
 <?php
 
-use App\Http\Controllers\MembroController;
-use App\Http\Controllers\ParceiroController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('index');
-});
-
-Route::get('/about', function () {
-    return view('about');
-});
-
-Route::get('/pricing', function () {
-    return view('pricing');
-});
-
-Route::get('/contact', function () {
-    return view('contact');
+    return view('welcome');
 });
 
 Route::get('/dashboard', function () {
@@ -29,8 +15,6 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    Route::resource('membros', MembroController::class);
-    Route::resource('parceiros', ParceiroController::class);
 });
 
 require __DIR__.'/auth.php';
