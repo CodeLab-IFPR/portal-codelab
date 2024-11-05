c49212e063fce8c667a3d16663d9eb8993d129bd
caduHD4 <cadumarianogarciapereira@gmail.com>
2024-11-05 15:35:41 -0300

    Adiciona suporte para estilo de login personalizado, traduz mensagens de autenticação para português.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,45 +1,27 @@
-<x-guest-layout>
-    <!-- Status da Sessão -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
-
-    <form method="POST" action="{{ route('login') }}">
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Formulário de Login e Registro</title>
+  <!---Custom CSS File--->
+  @vite('resources/css/styleLogin.css')    
+</head>
+<body>
+  <div class="container">
+    <input type="checkbox" id="check">
+    <div class="login form">
+      <header>Login</header>
+      <form method="POST" action="{{ route('login') }}">
         @csrf
-
-        <!-- Endereço de Email -->
-        <div>
-            <x-input-label for="email" :value="__('E-mail')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
-
-        <!-- Senha -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Senha')" />
-
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="current-password" />
-
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
-
-        <!-- Lembrar-me -->
-        <div class="block mt-4">
-            <label for="remember_me" class="inline-flex items-center">
-                <input id="remember_me" type="checkbox" class="rounded dark:bg-gray-900 border-gray-300 dark:border-gray-700 text-indigo-600 shadow-sm focus:ring-indigo-500 dark:focus:ring-indigo-600 dark:focus:ring-offset-gray-800" name="remember">
-                <span class="ms-2 text-sm text-gray-600 dark:text-gray-400">{{ __('Lembrar da senha') }}</span>
-            </label>
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('password.request') }}">
-                {{ __('Esqueci minha senha') }}
-            </a>
-
-            <x-primary-button class="ms-3">
-                {{ __('Entrar') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
+        <input type="email" name="email" placeholder="Digite seu e-mail" :value="old('email')" required autofocus autocomplete="username">
+        <input type="password" name="password" placeholder="Digite sua senha" required autocomplete="current-password">
+        <a href="{{ route('password.request') }}">Esqueceu a senha?</a>
+        <input type="submit" class="button" value="Entrar">
+      </form>
+      </div>
+    </div>
+  </div>
+</body>
+</html>
