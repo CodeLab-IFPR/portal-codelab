2a8900f24d0caa476e53d64205ed1472e0d2f240
Isaque Dias <isaque.brain@gmail.com>
2025-07-20 13:16:36 -0300

    feat: adiciona tags nas views de projetos

--- resources/views/projetos/create.blade.php
+++ resources/views/projetos/create.blade.php
@@ -42,6 +42,32 @@ Projeto - Criar
                     <div class="form-text text-danger">{{ $message }}</div>
                 @enderror
             </div>
+
+            <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0/dist/css/select2.min.css" rel="stylesheet" />
+            <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0/dist/js/select2.min.js"></script>
+
+            <div class="mb-3">
+                <label for="inputTags" class="form-label"><strong>Tags:</strong></label>
+                <select name="tags[]" id="inputTags" class="form-control border-primary @error('tags') inválido @enderror" multiple>
+                    @foreach($tags as $tag)
+                        <option value="{{ $tag->id }}">{{ $tag->name }}</option>
+                    @endforeach
+                </select>
+                @error('tags')
+                    <div class="form-text text-danger">{{ $message }}</div>
+                @enderror
+            </div>
+
+            <script>
+                document.addEventListener('DOMContentLoaded', function () {
+                    $('#inputTags').select2({
+                        placeholder: "Selecione tags",
+                        allowClear: true,
+                        closeOnSelect: false // Prevent closing the dropdown when selecting an item
+                    });
+                });
+            </script>
+
             <div class="mb-3">
                 <label for="inputStatus" class="form-label"><strong>Status:</strong></label>
                 <div class="form-check">
