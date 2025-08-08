f2f995aae6d7f7852872e99580df18f6b1258c52
Isaque Dias <isaque.brain@gmail.com>
2025-08-08 16:58:05 -0300

    ajustes nos cards de projetos

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,7 +10,7 @@ CodeLab IFPR
 
         <!-- Hero Content-->
         <div class="container pt-4 pt-md-6 pt-lg-8 pb-8 pb-lg-10 position-relative">
-            <div class="row gx-10 mx-0 align-items-center justify-content-center">
+            <div class="row align-items-center justify-content-center">
 
             <!-- Hero Text-->
             <div class="col-12 col-lg-8 position-relative z-index-20 text-center" data-aos="fade-in">
@@ -265,7 +265,7 @@ CodeLab IFPR
     <div class="container">
         <h4 class="fs-1 fw-bold mb-6 text-white text-center">Últimos projetos</h4>
 
-       <div class="row row-cols-1 row-cols-md-2 row-cols-xl-3 g-5">
+       <div class="row row-cols-1 row-cols-md-2 row-cols-xl-3 g-4">
     @foreach($projetos as $projeto)
     <style>
     @media (max-width: 767px) {
@@ -566,7 +566,7 @@ CodeLab IFPR
                             <img src="{{ asset($projeto->imagem) }}" alt="{{ $projeto->nome }}" class="img-fluid rounded-4" style="width: 100%; height: 100%; object-fit: cover;">
                         </div>
                         @else
-                        <img src="https://avatars.githubusercontent.com/u/217792933?s=200&v=4" alt="Default Image" class="img-fluid rounded-4">
+                        <img src="https://avatars.githubusercontent.com/u/217792933?s=200&v=4" alt="Default Image" class="img-fluid rounded-4 flex-shrink-0" style="height: 220px; object-fit: contain; width: 100%; border: 2px solid #e9ecef;">
                         @endif
                         <div class="mt-4 mb-4">
                             <p class="fw-bold mb-0">Palavras-chave</p>
