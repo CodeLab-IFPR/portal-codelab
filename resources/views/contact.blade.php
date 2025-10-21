b91335fc5805b8468eed2c02baca12e948437b09
Isaque Dias <isaque.brain@gmail.com>
2025-10-21 13:38:54 -0300

    fix(contato): corrige envio de email

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -7,16 +7,16 @@ Contate Nos
 <!-- Titulo -->
 
 @section('content')
-<header class="pt-10">
+<!-- <header class="pt-10">
     <div class="container">
         <div class="text-center col-12 col-sm-9 col-lg-7 col-xl-6 mx-auto position-relative z-index-20">
             <h1 class="display-3 fw-bold mb-3">Entre em contato</h1>
             <p class="text-muted lead mb-0">Selecione uma categoria abaixo para enviar um e-mail para a equipe de suporte correta, ou, alternativamente, envie-nos uma mensagem geral usando o formulário abaixo.</p>
         </div>
     </div>
-</header>
-<div class="container position-relative z-index-20 py-7">
-    <div class="row gx-10 my-10 pt-5">
+</header> -->
+<div class="container position-relative z-index-20 py-0">
+    <div class="row gx-10 mb-10 pt-5">
         <div class="col-12 col-lg-8">
             <p class="mb-3 small fw-bolder tracking-wider text-uppercase text-primary">Entre em contato</p>
             <h2 class="display-5 fw-bold mb-6">Envie-nos uma mensagem</h2>
